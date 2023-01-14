430228919bfde0f3612782ecbc4f378bf996e27b
HYPER <meerhadikhan364@gmail.com>
2023-01-14 16:52:46 +0500

    get height

--- lib/view/first_view.dart
+++ lib/view/first_view.dart
@@ -11,6 +11,7 @@ class FirstView extends StatefulWidget {
 class _FirstViewState extends State<FirstView> {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 1;
     return Scaffold(
       body: Center(
           child: Column(
@@ -23,6 +24,24 @@ class _FirstViewState extends State<FirstView> {
             child: Text("Go back"),
           ),
           Text("Hello word"),
+          Container(
+            height: 100,
+            color: Colors.green,
+            width: double.infinity,
+            child: Center(child: Text("text")),
+          ),
+          Container(
+            height: Get.height * 0.2,
+            color: Colors.red,
+            width: double.infinity,
+            child: Center(child: Text("Get Height")),
+          ),
+          Container(
+            height: height * 0.1,
+            color: Colors.brown,
+            width: double.infinity,
+            child: Center(child: Text("Media Query height")),
+          ),
         ],
       )),
     );
